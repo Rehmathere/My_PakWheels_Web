a1e3f14d5463a22a14045f9f3877ffdf518ff20f
REHMAT QAZI <103884066+Rehmathere@users.noreply.github.com>
2024-08-30 17:05:53 +0500

    Add files via upload

--- AutoFinder_Web/src/pages/user/userProfile/userProfile.js
+++ AutoFinder_Web/src/pages/user/userProfile/userProfile.js
@@ -10,6 +10,7 @@ const UserProfile = () => {
     <div className="UserProfile" style={{height:"50vh"}}>
       <h1 style={{textAlign:"center"}}>User Profile</h1>
       <div style={{border:"none" , display:"flex" , flexDirection:"column",width:"40%" , margin:"auto"}}>
+        <p><b>User ID : </b> {user?user._id:""} </p>
         <p><b>Name : </b> {user?user.name:""} </p>
         <p><b>Email : </b> {user?user.email:""} </p>
         <p><b>Phone Number : </b> {user?user.phoneNumber:""} </p>
